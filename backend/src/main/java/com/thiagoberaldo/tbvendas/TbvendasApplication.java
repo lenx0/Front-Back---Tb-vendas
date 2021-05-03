cc77e6eefb775fc380d3c6b35165949b1aed21b4
Thiago <thiagofreitas1106@gmail.com>
2021-05-03 14:30:20 -0300

    Project created

---
+++ backend/src/main/java/com/thiagoberaldo/tbvendas/TbvendasApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoberaldo.tbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thiagoberaldo/tbvendas/TbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoberaldo.tbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
