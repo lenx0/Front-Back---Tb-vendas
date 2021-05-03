e456f7bdba7c2c5b4377e4cf6168aa5dc2e65aae
Thiago <thiagofreitas1106@gmail.com>
2021-05-03 20:03:35 -0300

    img changed

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,11 @@
-import ImgDsDark from 'assets/img/ds_dark.svg';
+//import ImgDsDark from 'assets/img/ds_dark.svg';
+import sales from 'assets/img/sales.jpg'
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                    <img src={sales} alt="sales" width="120" />
                 </nav>
             </div>
         </div>
