92dc1ae8a7649e6b1db46e5da5a1710068ecf9af
Thiago <thiagofreitas1106@gmail.com>
2021-05-03 18:43:07 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/thiago-de-freitas-beraldo-06515a163/" target="_blank" rel="noreferrer">Thiago Beraldo</a></p>
+                <p className="text-light"><small><strong>Aplicativo de Vendas</strong><br />
+      Acesse já: <a href="https://www.facebook.com/TB-Dev-102382395353832" target="_blank" rel="noreferrer">Facebook</a></small></p>
+            </div>
+        </footer>
+
+
+    );
+}
+
+export default Footer;
